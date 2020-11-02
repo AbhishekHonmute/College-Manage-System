89a25352ecc9983c4a12d0f9d4439f67a7873375
Abhishek Honmute <honmuteabhishek@gmail.com>
2020-11-02 22:29:49 +0530

    Delete set_session.php

--- dbms_project/dashboard/set_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(isset($_POST['session_name'])) {
-        $_SESSION[$_POST['session_name']] = $_POST['session_value'];
-    }
-    // if(isset($_SESSION['session_name'])) {
-    //     echo $_SESSION['session_name'];
-    // }
-?>
